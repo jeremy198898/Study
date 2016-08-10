459870d252e63b3b826a98c18c506fa6f2a859ed
jeremy <jeremy@zhangzhide.com>
2016-08-10 13:26:35 +0800

    master

--- stuff/db/mysql.sql
+++ stuff/db/mysql.sql
@@ -1,18 +1,8 @@
---Mysql
-
---- table t_department
-
-CREATE TABLE t_department
-(
-  id									BIGINT UNIQUE DEFAULT NEXTVAL('seq_users') NOT NULL ,
-
-  orgid								INTEGER NOT NULL DEFAULT 0,															-- 组织ID
-
-  username            VARCHAR(100) NOT NULL,                                  -- 用户名
-  nickname            VARCHAR(100) DEFAULT '',                                -- 昵称
-  password            VARCHAR(50) DEFAULT '',                                 -- 密码
-
-  del									BOOLEAN NOT NULL DEFAULT false ,
-  time								TIMESTAMP(0) NOT NULL DEFAULT CURRENT_TIMESTAMP , 		  -- 订单生成时间
+CREATE TABLE
+IF NOT EXISTS t_users (
+  id BIGINT UNIQUE NOT NULL,
+  username VARCHAR (100) NOT NULL,
+  password VARCHAR (50) NOT NULL,
+  time TIMESTAMP (0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)
 );
