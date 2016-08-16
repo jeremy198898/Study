65909c65e3d287cd0ba25b2d78cec54c4fbaf793
jeremy <jeremy@zhangzhide.com>
2016-08-16 14:23:27 +0800

    master

--- tools/src/test/tests/OssTest.java
+++ tools/src/test/tests/OssTest.java
@@ -1,9 +1,24 @@
 package tests;
 
+import alioss.AliyunOssClient;
+import org.junit.Test;
+
+import java.io.File;
+
 /**
  * Created by jeremy on 2016/8/15.
  */
 public class OssTest
 {
+    @Test
+    public void test()
+    {
+        String filePath ="D:\\Study.zip";
+        File file = new File(filePath);
 
+        AliyunOssClient client = new AliyunOssClient();
+        boolean x = client.uploadFile(filePath, "test/1.zip");
+
+        System.out.println(x);
+    }
 }
